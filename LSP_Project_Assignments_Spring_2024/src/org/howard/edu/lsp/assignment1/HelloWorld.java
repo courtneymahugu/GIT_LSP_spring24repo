36c4cd442f6be27504597651b435fcc91e3a030c
courtneymahugu <courtneymahugu@gmail.com>
2024-01-19 21:38:28 -0500

    This is Assignment #1

---
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+*  Name: Courtney Mahugu
+*/
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+
+}
---
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/assignment2/HelloWorld2.java
@@ -0,0 +1,10 @@
+/**
+*  Name: Courtney Mahugu
+*/
+package org.howard.edu.lsp.assignment2;
+
+public class HelloWorld2 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
