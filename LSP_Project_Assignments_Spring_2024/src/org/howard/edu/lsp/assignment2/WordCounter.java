347a32181cd7ff693e131f28cf7c7108805615f0
courtneymahugu <courtneymahugu@gmail.com>
2024-02-05 15:51:17 -0500

    Added comments and removed helloworld file

--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/assignment2/WordCounter.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/assignment2/WordCounter.java
@@ -1,3 +1,6 @@
+/**
+*  Name: Courtney Mahugu
+*/
 package org.howard.edu.lsp.assignment2;
 
 import java.nio.file.Files;
@@ -10,43 +13,40 @@ import java.util.Map;
 public class WordCounter {
 
 	public static void main(String[] args) {
-        		
+
 		try {
 
-			Map<String, Integer> tracker = new HashMap<>();
+			Map<String, Integer> tracker = new HashMap<>(); //was going to use a HashSet but wanted value pairs
 
-			List<String> lines = Files.readAllLines(Paths.get("src/org/howard/edu/lsp/assignment2/words.txt"));
+			List<String> lines = Files.readAllLines(Paths.get("src/org/howard/edu/lsp/assignment2/words.txt")); //takes all the lines and stores line as a value in a list
 
 			for (String line : lines) {
-				
-		
-				String[] lineToWords = line.split("[' ]+");
-				
-				for (String word : lineToWords) {
-					
-					word = word.replaceAll("[^a-zA-Z']", ""); //Removes anything that's not in the alphabet
-					word = word.toLowerCase();
-					
-					if(word.isEmpty() || word.length() < 4) {
-				        continue;
 
-				    } else if(!tracker.containsKey(word)) {
-	
+				String[] lineToWords = line.split("[' ]+"); // splits the line into words, separates by space AND apostrophes (to separate contractions)
+
+				for (String word : lineToWords) {
+
+					word = word.replaceAll("[^a-zA-Z']", ""); // Removes anything that's not in the alphabet (aka. numbers)
+					word = word.toLowerCase(); //to avoid case-sensitivity
+
+					if (word.isEmpty() || word.length() < 4) {
+						continue;
+
+					} else if (!tracker.containsKey(word)) { //if not already counted, start counter
+
 						tracker.put(word, 1);
-	
-					} else {
-	
-						int currentValue = tracker.getOrDefault(word, 0);
+
+					} else { //add to count
+
+						int currentValue = tracker.getOrDefault(word, 0); //would set value to 0 if no value
 						tracker.put(word, currentValue + 1);
 					}
 				}
 			}
-			
-			
+
 			for (Map.Entry<String, Integer> entry : tracker.entrySet()) {
-	            System.out.println(entry.getKey() + "  " + entry.getValue());
-	        }
-			
+				System.out.println(entry.getKey() + "  " + entry.getValue());
+			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
